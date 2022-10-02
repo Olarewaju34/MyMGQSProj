426009ff0cb1107cae2a1a6910abc96fae957103
absyinka <absyinka@gmail.com>
2022-10-02 09:51:50 +0100

    Add project files.

---
+++ Constants/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MGQSSimpleEmployeeAppFile.Constants
+{
+    public class Constants
+    {
+        public const string dir = @"C:\Users\admin\Desktop\employeeRecord";
+        public const string filename = "employees.txt";
+        public static string fullPath = dir+"\\"+filename;
+    }
+}
